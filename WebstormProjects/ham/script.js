4923c57b6081042df2441b10a4e9b990f1ab286a
LeQuangNghia <lequangnghia35@gmail.com>
2019-01-15 02:47:24 +0700

    ab

---
+++ WebstormProjects/ham/script.js
@@ -0,0 +1,8 @@
+function  square(number) {
+    var result = number*number;
+    return result;
+}
+var square02 = square(2);
+document.write("square of 2 this  :"  +square02+ "<br/>");
+var square05 = square(5);
+document.write("square of 5 this : "  +square05);
